827473b53f25a9d12fd3595f07497f0f73e9b933
Alex Vo <voa@voa-a01.vmware.com>
2022-07-28 13:23:59 -0700

    1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return jsonify(
+        {"hello": "world"}
+    )
 
 
 if __name__ == '__main__':
