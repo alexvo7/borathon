1bfb60be6f4e0be6f6f0bd2f7ab32e71df55c0f7
Alex Vo <voa@voa-a01.vmware.com>
2022-07-29 08:48:34 -0700

    2

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient(port=27017)
+db = client.borathon
+accounts = db.accounts
+transactions = db.transactions
 
 
 @app.route('/')
@@ -8,6 +13,45 @@ def hello_world():  # put application's code here
     return jsonify(
         {"hello": "world"}
     )
+
+
+@app.route('/api/customer/<id>', methods=["GET"])
+def get_customer_acc(id):
+    return {"hello": "world"}
+
+
+@app.route('/api/customer/open', methods=["POST"])
+def open_acc():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print(type(json), json)
+        # post = accounts.insert_one(
+        #     {
+        #         ""
+        #     }
+        # )
+    return "Content-Type not supported!"
+
+
+@app.route('/api/customer/close', methods=["POST"])
+def close_acc():
+    return {"hello": "world"}
+
+
+@app.route('/api/customer/apply', methods=["POST"])
+def apply_transaction():
+    return {"hello": "world"}
+
+
+@app.route('/customer/', methods=["GET"])
+def customer_table():
+    return {"hello": "world"}
+
+
+@app.route('/transaction/', methods=["GET"])
+def transaction_table():
+    return {"hello": "world"}
 
 
 if __name__ == '__main__':
